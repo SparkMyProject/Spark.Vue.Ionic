d3e02c5fdbbe57684321667d5b9b65552aabae31
Violet Lauro <vinniehat@gmail.com>
2023-04-26 10:59:39 -0400

    Made routing more dynamic

---
+++ src/router/AppPages.ts
@@ -0,0 +1,51 @@
+import {
+    archiveOutline, archiveSharp,
+    heartOutline,
+    heartSharp,
+    mailOutline,
+    mailSharp,
+    paperPlaneOutline,
+    paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp
+} from "ionicons/icons";
+
+const appPages = [
+    {
+        title: 'Inbox',
+        url: '/folder/Inbox',
+        iosIcon: mailOutline,
+        mdIcon: mailSharp,
+    },
+    {
+        title: 'Outbox',
+        url: '/folder/Outbox',
+        iosIcon: paperPlaneOutline,
+        mdIcon: paperPlaneSharp,
+    },
+    {
+        title: 'Favorites',
+        url: '/folder/Favorites',
+        iosIcon: heartOutline,
+        mdIcon: heartSharp,
+    },
+    {
+        title: 'Archived',
+        url: '/folder/Archived',
+        iosIcon: archiveOutline,
+        mdIcon: archiveSharp,
+    },
+    {
+        title: 'Trash',
+        url: '/folder/Trash',
+        iosIcon: trashOutline,
+        mdIcon: trashSharp,
+    },
+    {
+        title: 'Spam',
+        url: '/folder/Spam',
+        iosIcon: warningOutline,
+        mdIcon: warningSharp,
+    },
+];
+const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+export default appPages;
