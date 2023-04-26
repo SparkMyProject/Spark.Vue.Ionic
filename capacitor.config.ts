bff709c3e634fb6cbd9fca78e50a7a5e83266ab7
Violet Lauro <vinniehat@gmail.com>
2023-04-26 10:21:13 -0400

    Moved files out of sub folder

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sparkles.Moble.Ionic.Vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
